b959fa9212fe1a22db81ee515e5a44d8c9b40631
PRO <54608551+PRO-2684@users.noreply.github.com>
2020-02-05 11:54:33 +0800

    Path Change

--- Netease Music.py
+++ Netease Music.py
@@ -1,5 +1,6 @@
 from requests import get
 from random import choice
+path = r'E:\Music'
 num = '0123456789'
 heads = ({'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1'},
          {'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; HUAWEI NXT-TL00 Build/HUAWEINXT-TL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/045008 Mobile Safari/537.36 V1_AND_SQ_8.2.0_1296_YYB_D QQ/8.2.0.4310 NetType/4G WebP/0.3.0 Pixel/1080 StatusBarHeight/66 SimpleUISwitch/1'})
@@ -39,6 +40,6 @@ if id:
     url = down_url.format(id)
     name = input('name: ')
     if name: id = name
-    path = f'/storage/F655-DA70/Music/{id}.mp3'
+    path += '\\' + id + '.mp3'
     download(url, path, head)
 
